eb244340e89c663857c28484deb3825ea62ec091
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-11-05 11:20:01 +0530

    #feat:Added deployed frontend link in bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -178,8 +178,10 @@ exports.makeBookPaymentController = async (req,res)=>{
     payment_method_types:["card"],
     line_items,
     mode: 'payment',
-    success_url: 'http://localhost:5173/payment-success',
-    cancel_url:"http://localhost:5173/payment-error"
+    // success_url: 'http://localhost:5173/payment-success',
+    // cancel_url:"http://localhost:5173/payment-error"
+    success_url: 'https://mern-book-store-frontend-omega.vercel.app/payment-success',
+    cancel_url:"https://mern-book-store-frontend-omega.vercel.app/payment-error"
 
 }); 
 console.log(session);
